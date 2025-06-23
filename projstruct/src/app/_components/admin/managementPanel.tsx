a4be75e62d41bdea7e763ee6b40bb6008964e967
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2025-06-23 20:15:16 +0000

    admin front primeira parte #5

---
+++ projstruct/src/app/_components/admin/managementPanel.tsx
@@ -0,0 +1,19 @@
+import { Table } from "~/app/_components/admin/table"
+import { FaPlus } from "react-icons/fa6";
+
+
+
+
+export function ManagementPanel({nome}: {nome: string}) {
+    return (
+        <div className="border-2 rounded-md border-gray-200 bg-white m-4">
+            <div className="flex justify-between m-2 text-lg p-2">
+                <h2 className="font-bold">{nome}</h2>
+                <button className="bg-purple-500 text-white rounded-sm pl-2 pr-2 p-1 text-sm flex items-center justify-evenly"><FaPlus className="mr-2" />Adicionar Produto</button>
+            </div>
+            <div className="flex justify-center">
+                <Table nome={nome}></Table>
+            </div>
+        </div>
+    )
+}
