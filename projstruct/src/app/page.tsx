4aa4f5670b072d0de5fab675b0ebb625f285f5d3
Luisgds <luisgdscat10@gmail.com>
2025-06-23 16:35:12 -0300

    #1 começando as divs

--- projstruct/src/app/page.tsx
+++ projstruct/src/app/page.tsx
@@ -4,9 +4,31 @@ import { Card } from "./_componentshomepage/card";
 export default function Homepage() {
   return (
     <main>
+      <div className="items-center bg-purple-200 flex max-w-lm p-6">
+        <div className="my-auto flex text-2xl flow-root ">
+          <h1 >Sua Música, </h1>
+          <h1 className="text-purple-300">
+            Nossa paixão
+          </h1>
+        </div>
+        <div>
+          <p>
+            Descubra os melhores instrumentos musicas, equippamentos de áudio e acessórios.
+             Transforme sua paixa pela música em realidade.
+          </p>
+        </div>
+        <div>
+          <h1> Bem vindos</h1>
+        </div>
+      </div>
       <div className="w-100vh h-100vh bg-blue-400 mx-auto flex max-w-sm">
         <h1> Bem vindos</h1>
         
+        <Card/>
+      </div>
+      <div className="w-100vh h-100vh bg-black mx-auto flex max-w-sm">
+        <h1> Bem vindos</h1>
+        <Card/>
         <Card/>
       </div>
     </main>
