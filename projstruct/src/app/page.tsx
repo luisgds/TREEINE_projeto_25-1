d6055d816613612cf1f3d27bb9e278472fff97de
dendo1234 <37016869+dendo1234@users.noreply.github.com>
2025-06-23 23:14:10 +0000

    arrumando home page

--- projstruct/src/app/page.tsx
+++ projstruct/src/app/page.tsx
@@ -1,14 +1,9 @@
-import Link from "next/link";
-import {Card} from "./_components/_componentsProductPage/card"
-import { NavBar } from "./_components/navbar";
 
 export default function HomePage() {
   return (
     <main>
       <div className="w-100vh h-100vh bg-blue-400 mx-auto flex max-w-sm">
         <h1> Bem vindos</h1>
-        
-        <Card/>
       </div>
     </main>
   );
