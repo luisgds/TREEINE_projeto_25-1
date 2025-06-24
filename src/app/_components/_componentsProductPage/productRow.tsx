3e0228637c3d114ac03b7bd3c7c608c20a0c0ba1
Miner320 <pedrobsb0303@gmail.com>
2025-06-24 15:55:36 -0300

    pequenas mudanças no css da pagina de produtos

--- src/app/_components/_componentsProductPage/productRow.tsx
+++ src/app/_components/_componentsProductPage/productRow.tsx
@@ -4,10 +4,15 @@ import styles from "~/productPageStyles/productrow.module.css"
 
 export function ProductRow(){
     return(
-        <div className="w-4/5 flex justify-evenly m-auto max-[1024px]:flex-col flex-wrap items-center">
+        <div className="w-4/5 flex justify-center m-auto max-[1024px]:flex-col flex-wrap items-center">
             <Card nome="teste" preco={124}></Card>
             <Card nome="teste" preco={90}></Card>
             <Card nome="teste" preco={699}></Card>
+            <Card nome="teste" preco={699}></Card>
+            <Card nome="teste" preco={699}></Card>
+            <Card nome="teste" preco={699}></Card>
+            <Card nome="teste" preco={699}></Card>
+
         </div>
     );
 }
