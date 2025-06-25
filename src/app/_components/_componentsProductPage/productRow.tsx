10dcdb851ea5f36b1b4122b4da89e224dfaa1900
Miner320 <pedrobsb0303@gmail.com>
2025-06-25 13:48:40 -0300

    consertei responsividade na pagina de produtos

--- src/app/_components/_componentsProductPage/productRow.tsx
+++ src/app/_components/_componentsProductPage/productRow.tsx
@@ -4,7 +4,7 @@ import styles from "~/productPageStyles/productrow.module.css"
 
 export function ProductRow(){
     return(
-        <div className="w-4/5 flex justify-center items-center m-auto max-[1024px]:flex-col flex-wrap max-[600]:grid max-[600]:justify-normal">
+        <div className="w-4/5 flex justify-center items-center m-auto max-[1024px]:flex-col flex-wrap max-[600]:grid max-[600]:justify-center">
             <Card nome="teste" preco={124}></Card>
             <Card nome="teste" preco={90}></Card>
             <Card nome="teste" preco={699}></Card>
--- src/app/_components/_componentsProductPage/segundotexto.tsx
+++ src/app/_components/_componentsProductPage/segundotexto.tsx
@@ -6,7 +6,7 @@ export function SegundoTexto(){
         <div className="m-auto flex flex-col text-center p-12">
             <h2 className="text-2xl font-normal">Sobre nós</h2>
             <p className="w-8/10 m-auto mb-4 text-gray-600">Mais de 20 anos dedicados à música</p>
-            <div className="min-w-48 mt-8 bg-gray-100 rounded-2xl m-auto w-7/10 max-[1024]:flex max-[1024]:flex-col max-[1024]:flex-wrap ">
+            <div className="min-w-48 p-4 flex mt-8 bg-gray-100 rounded-2xl m-auto w-7/10 max-[1024]:flex-col max-[1024]:flex-wrap max-[600]:p-0">
                 <p className="w-4/10 p-8 pt-16 max-[1024]:w-10/10">Somos uma empresa familiar apaixonada por música. Desde 2004, oferecemos
                     os melhores instrumentos musicais e equipamento de áudio em Brasília.
                 </p>
