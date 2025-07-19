3c43bc60bd1faf9e3ffdd02978fdd9d9c08d8bb6
PedroH <pedrobsb0303@gmail.com>
2025-07-19 14:18:15 -0300

    retirei uma chamada de api da pagina de produtos

--- src/app/_components/_componentsProductPage/productRow.tsx
+++ src/app/_components/_componentsProductPage/productRow.tsx
@@ -7,7 +7,6 @@ export async function ProductRow(){
 
 
     const productList = await api.products.getAllProducts()
-    const prod = await api.products.deleteProduct(3)
 
     return(
         <div className="w-4/5 flex justify-center items-center m-auto max-[1024px]:flex-col flex-wrap max-[600]:grid max-[600]:justify-center">
