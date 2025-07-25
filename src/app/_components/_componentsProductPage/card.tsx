b1b83989a696cc2bac0b0a3e324c73ab97d6d2d0
PedroH <pedrobsb0303@gmail.com>
2025-07-25 16:30:25 -0300

    cards de produto agora recebem imagem propria

--- src/app/_components/_componentsProductPage/card.tsx
+++ src/app/_components/_componentsProductPage/card.tsx
@@ -1,12 +1,15 @@
 import styles from "~/productPageStyles/card.module.css"
 
 
-export function Card({nome, preco}:{nome:string, preco:number}){
+export function Card({nome, preco, image_id}:{nome:string, preco:number, image_id:bigint}){
+
+    const image = `${image_id}.jpg`
+
     return (
         <div className="m-4 border-2 border-solid border-black rounded-xl w-72 table flex-col p-4 max-h-80 max-[1024]:m-4">
 
             <div className="w-9/10 m-auto object-contain h-40">
-                <img className="w-10/10 rounded h-10/10" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReg5C0xgdL5UyagnEPC26Pz4B4Uyi9xjCr7Q&s" alt="" />
+                <img className="w-10/10 rounded h-10/10" src={image} alt="" />
             </div>
 
             <div className="h-5/10 pt-4">
